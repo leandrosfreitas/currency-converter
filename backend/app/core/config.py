0d532ee27c6376d5f62e276060e4be58028f9740
leandrosfreitas <leandrosanfreitas@gmail.com>
2025-10-29 10:20:41 -0300

    atualizando a api e melhorando o gitignore

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,12 +7,6 @@ load_dotenv()
 # URL da API externa de câmbio, definida no .env como API_URL
 API_URL: str | None = os.getenv("API_URL")
 
-# URL de conexão com o banco de dados, definida no .env como DATABASE_URL
-DATABASE_URL: str | None = os.getenv("DATABASE_URL")
-
 # Validação obrigatória
 if not API_URL:
     raise RuntimeError("API_URL não definida no ambiente.")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL não definida no ambiente.")
